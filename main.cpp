9ac807fd4ed4fe731f0b45461e68fa8496ecb914
Mohtashim Khan <moto_khan@hotmail.com>
2022-02-01 18:28:29 -0700

    Added Working MakeFile, Setup/Tested Debugger

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
-#include<algorithm>
+#include "header.h"
 using namespace std;
 
 int main()
 {
-    
+    helloWorld();
     return 0;
+
+    vector<int> myvec;
+
 }
---
+++ message.cpp
@@ -0,0 +1,8 @@
+#include "header.h"
+
+int helloWorld()
+{
+    std::cout << "Hello More Changes World! \n";
+    std::cout << "Test The NEW DEBUGGER! \n";
+    return 0;
+}
