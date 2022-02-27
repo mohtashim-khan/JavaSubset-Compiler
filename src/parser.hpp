87ae39b5da788a2e8f183f5f59765c722150561d
Mohtashim Khan <moto_khan@hotmail.com>
2022-02-27 00:28:52 -0700

    Parsing Function Declarations

--- src/parser.hpp
+++ src/parser.hpp
@@ -31,9 +31,44 @@ private:
     std::vector<ParserToken>::iterator currToken;
     std::vector<ParserToken>::iterator nextToken;
 
+    Node resultTree;
+
+    Node parseStart();
+    Node parseLiteral();
+    Node parseType();
+    Node parseGlobalDeclarations(); //Infinite Recursion
+    Node parseGlobalDeclaration();
+    Node variableDeclaration();
+    Node identifier();
+    Node functionDeclaration();
+    Node functionHeader();
+    Node functionDeclarator();
+    Node formalParameterList(); //Infinite Recursion
+    Node formalParameter();
+    Node mainFunctionDeclaration();
+    Node mainFunctionDeclarator();
+    Node block();
+    Node blockStatements(); //Infinite Recursion
+    Node blockStatment();
+    Node statement();
+    Node statementExpression();
+    Node primary();
+    Node argumentList(); //Infinite Recursion
+    Node functionInvocation(); 
+    Node postFixExpression();
+    Node unaryExpression();
+    Node multiplicativeExpression(); //Infinite Recursion
+    Node additiveExpression(); //Infinite Recursion
+    Node relationalExpression(); //Infinite Recursion
+    Node equalityExpression(); //Infinite Recursion
+    Node conditionalAndExpression(); //Infinite Recursion
+    Node conditionalOrExpression(); //Infinite Recursion
+    Node assignmentExpression();
+    Node assignment();
+    Node expression();
+
 
     
-   int expectStart();
 
     
 
